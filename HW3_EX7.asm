893e25c862f1ae755ab999fd42c45a6e83b87d5a
Mercedes Roman Ruiz <122405877+mercedesromanruiz@users.noreply.github.com>
2023-01-26 20:49:39 -0600

    Update HW3_EX7.asm

--- HW3_EX7.asm
+++ HW3_EX7.asm
@@ -3,8 +3,10 @@
 
 #include <p18f4620.inc>
 
-LIGHT equ 0
 CNT0 equ 1
+CNT1 equ 2
+CNT2 equ 3
+LED equ 4
 
 ; --- Program ---
 	org 0x800
@@ -41,13 +43,60 @@ Half:
 	movwf PORTC
 	movlw 255
 	movwf PORTD
+	bcf PORTD, 7
 	return
 
 All:
 	movlw 255
 	movwf PORTC
 	movwf PORTD
+	call OffC
 	return
+
+Wait:	; --- Wait 500 ms (2,500,000 clocks)
+	movlw 25
+	movwf CNT2
+Loop2:
+	movlw 100
+	movwf CNT1
+Loop1:
+	movlw 100
+	movwf CNT0
+Loop0:
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	nop
+	decfsz CNT0, F
+	goto Loop0
+	decfsz	CNT1, F
+	goto Loop1
+	decfsz CNT2, F
+	goto Loop2
+	return
+
+OffC:
+	call Wait
+	bcf PORTC, 7
+	call Wait
+	bcf PORTC, 6
+	call Wait
+	bcf PORTC, 5
+	call Wait
+	bcf PORTC, 4
+	call Wait
+	bcf PORTC, 3
+	call Wait
+	bcf PORTC, 2
+	call Wait
+	bcf PORTC, 1
+	call Wait
+	bcf PORTC, 0
+	return
+
 
 end
 
