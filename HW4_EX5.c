373b758c6a04347a59f8ca76a451fed7a57b3e37
Mercedes Roman Ruiz <122405877+mercedesromanruiz@users.noreply.github.com>
2023-02-11 23:12:51 -0600

    Create HW4_EX5.c

---
+++ HW4_EX5.c
@@ -0,0 +1,60 @@
+const unsigned char MSG0[10] = "Balance: $" ;
+const unsigned char MSG1[4] = "Bet:";
+const unsigned char MSG2[6] = "Wheel:";
+unsigned int BET, COUNT, PRESS;
+
+#include <pic18.h>
+#include "LCD_PortD.c"
+
+void CheckButton() {
+	if(RB0) {BET = 0; PRESS = 1;}
+	if(RB1) {BET = 1; PRESS = 1;}
+	if(RB2) {BET = 2; PRESS = 1;}
+	if(RB3) {BET = 3; PRESS = 1;}
+	if(RB4) {BET = 4; PRESS = 1;}
+	if(RB5) {BET = 5; PRESS = 1;}
+	if(RB6) {BET = 6; PRESS = 1;}
+	if(RB7) {BET = 7; PRESS = 1;}
+}
+
+void Count_Mod8(unsigned int N) {
+	unsigned int i;
+	for(i = 0; i < 40 + N; i++) {
+		COUNT = (COUNT + 1) % 8;
+	}
+}
+
+void main(void) {
+	unsigned int i,j;
+	unsigned int TIME;
+
+	TRISA = 0;
+	TRISB = 0xFF;
+	TRISC = 0;
+	TRISD = 0;
+	TRISE = 0;
+	ADCON1 = 0x0F;	
+
+	LCD_Init();
+	unsigned int BANK;
+	BANK = 10;
+
+	LCD_Move(0,0);  for (i=0; i<10; i++) LCD_Write(MSG0[i]); 
+	LCD_Move(0,10); LCD_Out(BANK, 5, 0);
+	TIME = 0;
+
+	unsigned int N;
+	LCD_Move(1,0); for (i=0; i < 4; i++) LCD_Write(MSG1[i]);
+	LCD_Move(1,8); for (i=0; i < 6; i++) LCD_Write(MSG2[i]);
+	
+	while(1) {
+		CheckButton();
+		if(PRESS == 1) {
+			N = rand() % 8;
+			LCD_Move(1,4); LCD_Out(BET, 1, 0);
+			Count_Mod8(N);
+			LCD_Move(1,14); LCD_Out(COUNT, 1, 0);
+			PRESS = 0;
+		}
+	}
+}
