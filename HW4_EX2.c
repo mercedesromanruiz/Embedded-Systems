e8539f02f2e0ef9b25951fdc0e59a5be57799952
Mercedes Roman Ruiz <122405877+mercedesromanruiz@users.noreply.github.com>
2023-02-11 23:12:49 -0600

    Update HW4_EX2.c

--- HW4_EX2.c
+++ HW4_EX2.c
@@ -5,16 +5,6 @@ Exercise 2
 #include <pic18.h>
 
 
-void Beep(unsigned int FREQ, unsigned int DUR) {
-	unsigned int i, j, k;
-	for (i = 0; i < DUR; i++) {
-		for (j = 0; j < 1000; j++) {
-			i = (i + 1) % 42;
-			if (i == 0) PORTC += 1;
-		}
-	}
-}
-
 void main(void) {
 	TRISA = 0;
 	TRISB = 0;
@@ -22,7 +12,15 @@ void main(void) {
 	TRISD = 0;
 	TRISE = 0;
 	ADCON1 = 0x0F;
+
+	unsigned int i;
+	unsigned int j;
+	j = 100;
+	while(j > 0) {
+		RC0 = !RC0;
+		for (i=0; i<1561; i++);
+		j--;
+	}
 	
-	Beep(200,100);
 
 }
